809194150be9db70d7ee9204d56bdfa9d3411f00
gioiascari <gioia.ascari96@gmail.com>
2022-05-03 15:05:32 +0200

    email riuscita

--- snack2.php
+++ snack2.php
@@ -5,14 +5,14 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
 //  Passare come parametri GET name, mail e age 
     $name = $_GET["name"];
 
-    // $mail = $_GET["mail"];
+    $email = $_GET["email"];
 
     // $age = $_GET["age"];
 
 //  verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri 
 // che mail contenga un punto e una chiocciola e che age sia un numero.
 
-  if(strlen($name) > 3){
+  if(strlen($name) > 3 || strpos($email, '@') && strpos($email, '.')){
     echo ("Accesso Riuscito");
   }else{
     echo ("Accesso Negato");
