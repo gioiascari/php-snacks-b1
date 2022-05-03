c204a4b380ee03a9c2ce973ca16d38c2295429fb
gioiascari <gioia.ascari96@gmail.com>
2022-05-03 18:08:42 +0200

    modifiche varie

--- snack4.php
+++ snack4.php
@@ -1,7 +1,6 @@
 
 
 <?php 
-phpinfo();
 // Prendere un paragrafo abbastanza lungo, contenente diverse frasi.
     $text = "Lorem ipsum. dolor sit, amet consectetur adipisicing elit. Qui minima laudantium, ab deleniti quidem iure, odio eaque. Eius voluptate quos alias porro explicabo necessitatibus reprehenderit ducimus incidunt, quae quisquam eos.";
 // Prendere il paragrafo e suddividerlo in tanti paragrafi.
