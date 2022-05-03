cb3b0817b127ec5199ff73e520fa08e1ab2a0d68
gioiascari <gioia.ascari96@gmail.com>
2022-05-03 15:40:04 +0200

    snack3 completato

---
+++ snack3.php
@@ -0,0 +1,18 @@
+<!-- Creare un array con 15 numeri casuali, 
+tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<?php
+           $randomNumber =[];
+           while(count($randomNumber) <= 14){
+       
+               $random = rand(1,50);
+       
+               if(!in_array($random,$randomNumber)){
+       
+                   $randomNumber[] = $random;
+               };
+               
+           };
+           echo('<pre>');
+               var_dump($randomNumber);
+            echo('</pre>');
+    ?>
